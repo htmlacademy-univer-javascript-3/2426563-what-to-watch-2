6e78a096633a950d421bba68ee4680c80795fa68
Мелихова Ксения <kmelikhova@data-platform.ru>
2023-11-02 13:32:38 +0300

    fix

--- src/pages/player/player.tsx
+++ src/pages/player/player.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Page404 from '../page404';
 import LOCALE from './player.locale';
 import { useAppSelector } from '../../hooks/hooks';
